9e476d857e8b70cf2b1c834ce551a757cc0fadc8
Priyanka1916J <52973913+Priyanka1916J@users.noreply.github.com>
2024-02-11 16:48:58 +0530

    Update Test_class.java

--- src/test/java/Day1/Test_class.java
+++ src/test/java/Day1/Test_class.java
@@ -4,7 +4,7 @@ public class Test_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("hi");
+ System.out.println("Hi I'm Priyanka");
 	}
 
 }
